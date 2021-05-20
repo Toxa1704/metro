73dc582830857a2c6403194a90ec3667163b6609
Toxa1704 <Toxa1704@outlook.com>
2021-05-20 17:23:08 +0300

    master

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,17 +9,17 @@
       <th scope="col">#</th>
       <th scope="col">Name</th>
       <th scope="col">Transfer</th>
-      <th scope="col">Number of in/out</th>
+      <th scope="col">Number of in out</th>
       <th score="col">Comment</th>
     </tr>
   </thead>
   <tbody>
-    @foreach(@blue_1 as $blue)
+    @foreach($blue_1 as $blue)
     <tr>
       <th scope="row">{{$blue -> id}}</th>
       <td>{{$blue -> name}}</td>
       <td>{{$blue -> transfer}}</td>
-      <td>{{$blue -> number_of_in/out}}</td>
+      <td>{{$blue -> number_of_in_out}}</td>
       <td>{{$blue -> comment}}</td>
     </tr>
     @endforeach
